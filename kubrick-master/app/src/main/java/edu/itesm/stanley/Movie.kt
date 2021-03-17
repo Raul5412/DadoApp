b686008b12fbcf64f8a3fdf6f73c982c4fed3977
Raúl González Cardona <raul_5412@hotmail.com>
2021-03-17 10:51:55 -0600

    First

--- kubrick-master/app/src/main/java/edu/itesm/stanley/Movie.kt
+++
@@ -1,8 +0,0 @@
-package edu.itesm.stanley
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-import java.io.Serializable
-
-
-data class Movie(val picture:Int, val titulo:String, val anio:String, val plot: String)
